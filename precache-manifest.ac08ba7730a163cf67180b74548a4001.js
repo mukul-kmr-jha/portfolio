cf81fef43d11713d43c49d5ae0a6458b8fe2d89c
mukul-kmr-jha <jimickeyji@gmail.com>
2020-09-04 18:24:05 +0100

    Updates

---
+++ precache-manifest.ac08ba7730a163cf67180b74548a4001.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8212ccb5d87d6cb1eea93837adc4ff0a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c6c0783be86eeab9132c",
+    "url": "/portfolio/static/css/main.d26efba0.chunk.css"
+  },
+  {
+    "revision": "a3c87166ab4ebb7c4d5c",
+    "url": "/portfolio/static/js/2.8d424c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8d424c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c0783be86eeab9132c",
+    "url": "/portfolio/static/js/main.ed2337dd.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "8bb9d94b1568cb97a07424340dddd149",
+    "url": "/portfolio/static/media/background.8bb9d94b.jpg"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/portfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "40b275dd78de658f60f05693582adb1d",
+    "url": "/portfolio/static/media/stackoverflow.40b275dd.png"
+  }
+]);
